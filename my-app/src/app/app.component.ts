182c58ec8c3ae05d8a331003c0850f5775d54673
Miguel <miguel.leonarte@pearson.com>
2019-08-20 10:12:12 -0400

    I've changed title

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Miguel\'s Tour of Heroes';
+  title = 'Miguelito\'s Tour of Heroes';
 }
